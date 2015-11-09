db2cef7c4b274603c2f4b6d24f30fdfe31679feb
benjaminifaught <benjaminifaught>
2015-11-09 10:14:15 -0500

    3rd commit, random number and new game upon click

--- js/app.js
+++ js/app.js
@@ -13,13 +13,33 @@ $(document).ready(function(){
   	});
 
   // new game & generate random number
-  var randomNum = 0;
-  function targetInt () {
-      randomNum = Math.floor((Math.random() * 100) + 1);
-    };
-  console.log(randomNum);
 
-  var newGame 
+
+    function targetInt () {
+      var randomNum = 0;
+      randomNum = Math.floor((Math.random() * 100) + 1);
+      console.log(randomNum);
+      return randomNum;
+    };
+
+    targetInt();
+
+    $('.new').click(function () {
+      event.preventDefault();
+      targetInt();
+    });
+  
+
+// guessfeedbackloop
+
+  function userGuess() {
+    var guess = 0;
+    guess = prompt("Guess the Number","Your Guess");
+    console.log(guess);
+    return userGuess;
+  }
+
+  userGuess();
 
 
 });
